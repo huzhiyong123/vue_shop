2e7d1bcbfbfc445d1bf35053f8f2b87ed6ffa32e
huzhiyong <745017878@qq.com>
2022-02-11 23:45:40 +0800

    登录代码

---
+++ src/router.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from './components/login.vue'
+import home from './components/home.vue'
+
+
+
+Vue.use(Router)
+
+
+const router = new Router({
+  routes: [
+
+
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: login
+    },
+    {
+      path: '/home',
+      component: home
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next();
+  const tokenstr = window.sessionStorage.getItem('token')
+  if (!tokenstr) return next('/login')
+  next()
+
+
+
+})
+
+export default router
